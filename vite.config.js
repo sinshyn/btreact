028bd95de0eaffb97b927dc5c18fae83f62a2c90
sinshyn <sinshyn11@gmail.com>
2025-03-22 17:03:01 +0700

    Deploy fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/btreact/", // Đổi thành tên repo của bạn
   plugins: [react()],
+  base: "/btreact/", // Tên repo trên GitHub
 });
