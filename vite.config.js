7eb737f9ac111fc44d70151f6b63199c2c2a8150
sinshyn <sinshyn11@gmail.com>
2025-03-22 16:43:40 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/btreact/", // Đổi thành tên repo của bạn
   plugins: [react()],
-})
+});
